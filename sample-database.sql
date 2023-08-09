4dad5398ad9734f661c835c7ffe3e6274703b6af
NatashaCamille <nnalubega@outlook.com>
2023-08-09 23:33:17 +0300

    Some changes

--- sample-database.sql
+++ sample-database.sql
@@ -3,4 +3,7 @@ This is my mysql_sample_databse_schema
 
 CREATE DATABASE database_name; - CREATE DATABASE sample_database;
 
+CREATE TABLE table_name (
+    column1 datatype [optional_constraints],
+    column2 datatype [optional_constraints],
 
