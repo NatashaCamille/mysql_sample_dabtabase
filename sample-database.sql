60ed44c0f2ab107d14c6a2b2075843a931d23f11
NatashaCamille <nnalubega@outlook.com>
2023-08-09 23:33:50 +0300

    Some changes

--- sample-database.sql
+++ sample-database.sql
@@ -6,4 +6,6 @@ CREATE DATABASE database_name; - CREATE DATABASE sample_database;
 CREATE TABLE table_name (
     column1 datatype [optional_constraints],
     column2 datatype [optional_constraints],
+    optional_table_constraints
+); 
 
