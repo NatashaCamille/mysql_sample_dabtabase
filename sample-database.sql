b9abbc7a0a7bb85bb065fc9c7117d37a4baa68b9
NatashaCamille <nnalubega@outlook.com>
2023-08-27 22:19:55 +0300

    Updates

--- sample-database.sql
+++ sample-database.sql
@@ -10,20 +10,28 @@ CREATE TABLE table_name (
 ); 
 -- Table 1
 - CREATE TABLE Student (
-registration_number VARCHAR(50)  NOT NULL PRIMARY KEY,
-first_name VARCHAR(50) NOT NULL,
-last_name VARCHAR(50) NOT NULL,
-gender CHAR(5) NOT NULL,
-DOB VARCHAR(25) NOT NULL,
-email VARCHAR(100) NULL,
-phone_number VARCHAR(30) NOT NULL,
-marital_status VARCHAR(15) NULL,
-blood_type VARCHAR(3),
-nationality VARCHAR(25) NOT NULL,
-city VARCHAR(25) NOT NULL,
-home_address VARCHAR(25) NOT NULL,
-NIN VARCHAR(40) NOT NULL, 
-course_id VARCHAR (25) NOT NULL
+    registration_number VARCHAR(50)  NOT NULL PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    gender CHAR(5) NOT NULL,
+    DOB VARCHAR(25) NOT NULL,
+    email VARCHAR(100) NULL,
+    phone_number VARCHAR(30) NOT NULL,
+    marital_status VARCHAR(15) NULL,
+    blood_type VARCHAR(3),
+    nationality VARCHAR(25) NOT NULL,
+    city VARCHAR(25) NOT NULL,
+    home_address VARCHAR(25) NOT NULL,
+    NIN VARCHAR(40) NOT NULL, 
+    course_id VARCHAR (25) NOT NULL
+);
+
+-- Table 2
+
+CREATE TABLE Course (
+    course_id VARCHAR (25) PRIMARY KEY NOT NULL,
+    course_name VARCHAR (50), 
+    course_dept VARCHAR (50),
 );
 ___________________________________________________________________________
 
