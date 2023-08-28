2cb3978b7893e162b0483eb3a68583920db6d239
NatashaCamille <nnalubega@outlook.com>
2023-08-29 00:07:16 +0300

    Updates

--- sample-database.sql
+++ sample-database.sql
@@ -13,15 +13,15 @@ CREATE TABLE Student (
     registration_number VARCHAR(50)  NOT NULL PRIMARY KEY,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
-    gender CHAR(5) NOT NULL,
-    DOB VARCHAR(25) NOT NULL,
+    gender CHAR(10) NOT NULL,
+    DOB DATE NOT NULL,
     email VARCHAR(100) NULL,
     phone_number VARCHAR(30) NOT NULL,
     marital_status VARCHAR(15) NULL,
     blood_type VARCHAR(3),
     nationality VARCHAR(25) NOT NULL,
     city VARCHAR(25) NOT NULL,
-    home_address VARCHAR(25) NOT NULL,
+    home_address VARCHAR(30) NOT NULL,
     NIN VARCHAR(40) NOT NULL, 
     course_id VARCHAR (25) NOT NULL
 );
@@ -78,4 +78,21 @@ INSERT INTO table_name (column1, column2) VALUES (value1, value2);
 
 -- Table 1
 
-INSERT INTO ()
+INSERT INTO Student (
+    registration_number,
+    first_name,
+    last_name,
+    gender,
+    DOB,
+    email,
+    phone_number,
+    marital_status,
+    blood_type,
+    nationality,
+    city,
+    home_address,
+    NIN,
+    course_id
+) VALUES (
+    
+)
